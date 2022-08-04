14b2ac2d6345a7cfa35ca9fc02369033e798e427
Kazuhisa Ishizaka <iszk1215@gmail.com>
2022-08-05 07:02:10 +0900

    Update

--- mora/server/static/public/mora_coverage.js
+++ mora/server/static/public/mora_coverage.js
@@ -45,32 +45,50 @@ import hljs from 'https://unpkg.com/@highlightjs/cdn-assets@11.5.1/es/highlight.
             }
         }
 
+        const checkSpan = (line) => {
+            // console.log("checkSpan: " + line)
+            let spans = []
+            let i = 0
+            while (i < line.length) {
+                const tmp = line.slice(i)
+                if (tmp.startsWith("<span")) {
+                    const e = tmp.indexOf(">")
+                    spans.push(line.slice(i, i + e + 1))
+                    i += e + 1
+                } else if (tmp.startsWith("</span>")) {
+                    spans.pop()
+                    i += "</span>".length
+                } else
+                    ++i
+            }
+            //console.log(spans)
+            return spans
+        }
+
+
         let lst = []
         let block = blockIter.next()
         let lastSpan = ""
         for (let i = 0; i < lines.length; ++i) {
             let lineno = i + 1
-            // console.log(lines[i])
-            if (lines[i].length > 0) {
-                let tmp = lines[i].trim()
-                let hasClosingSpan = tmp.endsWith(">")
-                if (tmp.startsWith("<")) {
-                    const pos = tmp.indexOf(">")
-                    lastSpan = tmp.slice(0, pos + 1)
-                } else {
-                    // console.log("add opening spane")
-                    lines[i] = lastSpan + lines[i]
-                }
-
-                if (!hasClosingSpan) {
-                    // console.log("add closing spane")
-                    lines[i] += "</span>"
+            //console.log(lines[i])
+            //console.log("lastSpan=" + lastSpan)
+            let line = lines[i]
+            if (line.length > 0) {
+                line = lastSpan + line
+                lastSpan = ""
+                const spans = checkSpan(line)
+                if (spans.length > 0) {
+                    for (let j = 0; j < spans.length; ++j) {
+                        line = line + "</span>"
+                        lastSpan += spans[j]
+                    }
                 }
             }
 
 
             let prefix = ('    ' + lineno).slice(-4)
-            let text = prefix + "  " + lines[i];
+            let text = prefix + "  " + line
 
             let color = ""
             while (block && lineno > block[1])
