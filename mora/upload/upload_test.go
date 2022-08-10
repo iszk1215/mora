63ee0ac0d72e3b36b9d7822d5e79f6e223914bf3
Kazuhisa Ishizaka <iszk1215@gmail.com>
2022-08-11 05:52:44 +0900

    Update

---
+++ mora/upload/upload_test.go
@@ -0,0 +1,18 @@
+package upload
+
+import (
+	"testing"
+	"testing/fstest"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_relativePathFromRoot(t *testing.T) {
+	fsys := fstest.MapFS{
+		"src/test.cc": &fstest.MapFile{},
+	}
+
+	got := relativePathFromRoot("/home/mora/test/src/test.cc", fsys)
+
+	assert.Equal(t, "src/test.cc", got)
+}
