50040d1c97f79e7cebc068f89a5325915b879a21
Kazuhisa Ishizaka <iszk1215@gmail.com>
2023-02-02 22:38:55 +0900

    Update

--- mora/server/coverage_provider.go
+++ mora/server/coverage_provider.go
@@ -28,41 +28,27 @@ func NewMoraCoverageProvider(store CoverageStore) *MoraCoverageProvider {
 	return p
 }
 
-func (p *MoraCoverageProvider) addOrMergeCoverage(cov *Coverage) (*Coverage, error) {
-	p.Lock()
-	defer p.Unlock()
-
-	if p.store == nil {
-		return cov, nil
-	}
-
-	found, err := p.store.FindRevision(cov.RepoID, cov.Revision)
-	if err != nil {
-		return nil, err
-	}
-
-	if found == nil {
-		return cov, nil
-	}
-
-	log.Print("Merge with ", found.ID)
-
-	cov.ID = found.ID
-	return mergeCoverage(found, cov)
-}
-
 func (p *MoraCoverageProvider) AddCoverage(cov *Coverage) error {
 	log.Print("AddCoverage")
-	cov, err := p.addOrMergeCoverage(cov)
-	if err != nil {
-		return err
-	}
 
 	if p.store == nil {
 		return nil
 	}
 
-	log.Print("AddCoverage: Put: cov.ID=", cov.ID)
+	found, err := p.store.FindRevision(cov.RepoID, cov.Revision)
+	if err != nil {
+		return err
+	}
 
+	if found != nil {
+		log.Print("Merge with ", found.ID)
+		cov.ID = found.ID
+		cov, err = mergeCoverage(found, cov)
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Print("AddCoverage: Put: cov.ID=", cov.ID)
 	return p.store.Put(cov)
 }
