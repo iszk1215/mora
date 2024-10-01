4d59df0fe3d9c22bad40ba78993f98e736171db7
Kazuhisa Ishizaka <iszk1215@gmail.com>
2024-10-01 23:54:59 +0900

    update: config

--- mora/core/client_config.go
+++ mora/core/client_config.go
@@ -12,16 +12,60 @@ type ClientConfig struct {
 	Token         string `toml:"token"`
 }
 
-func ReadClientConfig(filename string) (ClientConfig, error) {
+type ServerConfig struct {
+	URL  string
+	Port int
+}
+
+type RepositoryManagerConfig struct {
+	Driver         string `toml:"scm"`
+	URL            string
+	SecretFilename string `toml:"secret_file"`
+}
+
+type MoraConfig struct {
+	Server             ServerConfig
+	RepositoryManagers []RepositoryManagerConfig `toml:"scm"`
+	Client             ClientConfig
+	Debug              bool
+	DatabaseFilename   string
+}
+
+func ReadMoraConfig(filename string) (MoraConfig, error) {
 	b, err := os.ReadFile(filename)
+	if err != nil {
+		return MoraConfig{}, err
+	}
+
+	var config MoraConfig
+	if err := toml.Unmarshal(b, &config); err != nil {
+		return MoraConfig{}, err
+	}
+
+	config.DatabaseFilename = "mora.db"
+
+	return config, nil
+}
+
+func ReadClientConfig(filename string) (ClientConfig, error) {
+	config, err := ReadMoraConfig(filename)
 	if err != nil {
 		return ClientConfig{}, err
 	}
 
-	var config ClientConfig
-	if err := toml.Unmarshal(b, &config); err != nil {
-		return ClientConfig{}, err
-	}
+	return config.Client, nil
 
-	return config, nil
+	/*
+		b, err := os.ReadFile(filename)
+		if err != nil {
+			return ClientConfig{}, err
+		}
+
+		var config ClientConfig
+		if err := toml.Unmarshal(b, &config); err != nil {
+			return ClientConfig{}, err
+		}
+
+		return config, nil
+	*/
 }
