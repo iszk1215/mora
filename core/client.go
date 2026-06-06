cb5618ec7f1e1b1a1b69206ed76cd71c0760d3bc
Kazuhisa Ishizaka <iszk1215@gmail.com>
2026-06-06 17:29:35 +0900

    Fix response body leak: add defer resp.Body.Close()

--- core/client.go
+++ core/client.go
@@ -49,6 +49,7 @@ func (c *APIClientImpl) Do(method, path string, in any, out any) error {
 	if err != nil {
 		return err
 	}
+	defer resp.Body.Close()
 
 	msg, err := io.ReadAll(resp.Body)
 	if err != nil {
